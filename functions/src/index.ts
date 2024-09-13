8f99034110ef12d59e57567e30d36b5b137dc332
Leighton Miguel <leim56@hawaii.rr.com>
2024-09-12 14:27:40 -1000

    Update functions deploy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,17 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import { HttpsError, onCall } from "firebase-functions/v2/https";
+import { defineSecret } from "firebase-functions/params";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+const employerSignupKey = defineSecret("EMPLOYER_SIGNUP_KEY");
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+exports.helloWorld = onCall(
+  { cors: true, secrets: [employerSignupKey] },
+  () => {
+    try {
+      return {
+        data: "Hello World",
+      };
+    } catch (e) {
+      throw new HttpsError("internal", "Interal server error.");
+    }
+  }
+);
