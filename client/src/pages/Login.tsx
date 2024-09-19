6188470408fc21101302b9626e4fe45c87dae6d0
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-09-18 14:40:05 -1000

    Created Login and Signup Form basic structure and added them to login and signup page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,13 +2,14 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
+import LoginForm from "../components/LoginForm";
 import "../styles/styles.css";
 
 const Login = () => {
   return (
     <>
       <NavBar />
-      <p className="py-5">This is the login page</p>
+      <LoginForm />
       <Footer />
     </>
   );
