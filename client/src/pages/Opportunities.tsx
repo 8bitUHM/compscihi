f56dee6d37547584e8e4a0fa80c0ae0e37ff7543
Leighton Miguel <leim56@hawaii.rr.com>
2024-09-19 20:13:49 -1000

    Finish opportunity component

--- client/src/pages/Opportunities.tsx
+++ client/src/pages/Opportunities.tsx
@@ -12,7 +12,7 @@ type Opportunity = {
   location: string;
   pay?: number;
   payPer?: string;
-  jobType: "Full-time" | "Part-time" | "Contract" | "Internship";
+  jobType: "Full-time" | "Part-time" | "Contract" | "Internship" | "Co-op";
   description: string;
   qualifications: string[];
   skills: string[];
@@ -31,12 +31,63 @@ const Opportunities = () => {
           key={key}
           className="w-full  p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 "
         >
-          <a href="#">
-            <h5 className=" text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-              {opportunity.title}
-            </h5>
-          </a>
+          <div
+            id={`static-modal-${key}`}
+            data-modal-backdrop="static"
+            tabIndex={-1}
+            aria-hidden="false"
+            className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+          >
+            <div className="relative p-4 w-full max-w-2xl max-h-full">
+              <div className="relative bg-gray-100 rounded-lg shadow-xl dark:bg-gray-700">
+                <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                  <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                    Apply to {opportunity.title}
+                  </h3>
 
+                  <button
+                    type="button"
+                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    data-modal-hide={`static-modal-${key}`}
+                  >
+                    <svg
+                      className="w-3 h-3"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 14 14"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                      />
+                    </svg>
+                    <span className="sr-only">Close modal</span>
+                  </button>
+                </div>
+
+                {/* Modal body */}
+                <div className="p-4 md:p-5 space-y-4">
+                  <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    {opportunity.applicationInstructions}
+                  </p>
+                  {opportunity.applyLink ? (
+                    <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400 underline">
+                      <a target="_blank" href={opportunity.applyLink}>
+                        {opportunity.applyLink}
+                      </a>
+                    </p>
+                  ) : null}
+                </div>
+              </div>
+            </div>
+          </div>
+          <h5 className=" text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+            {opportunity.title}
+          </h5>
           <div>
             <p>
               <small className="text-gray-500">
@@ -46,12 +97,27 @@ const Opportunities = () => {
             </p>
           </div>
           <p>
+            {opportunity.pay && opportunity.payPer ? (
+              <small>
+                $
+                {opportunity.pay
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
+                per {opportunity.payPer} ·{" "}
+              </small>
+            ) : null}
             <small>{opportunity.jobType}</small>
           </p>
-          <p className="mb-2 mt-1 font-normal text-gray-500 dark:text-gray-400">
-            {opportunity.description}
-          </p>
-          <div className="mb-3">
+          <div className="text-gray-500 dark:text-gray-400 mb-2">
+            <p className="mt-1 mb-1 font-normal ">{opportunity.description}</p>
+
+            <ul className="list-disc ml-4">
+              {opportunity.qualifications.map((val, key) => (
+                <li key={key}>{val}</li>
+              ))}
+            </ul>
+          </div>
+          <div>
             {opportunity.skills.map((val, key) => (
               <span
                 key={key}
@@ -61,7 +127,23 @@ const Opportunities = () => {
               </span>
             ))}
           </div>
-          <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+          {opportunity.benefits ? (
+            <div className="">
+              {opportunity.benefits.map((val, key) => (
+                <span
+                  key={key}
+                  className="me-2 my-1 inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
+                >
+                  {val}
+                </span>
+              ))}
+            </div>
+          ) : null}
+          <button
+            className="relative inline-flex items-center justify-center p-0.5 mb-2 mt-3 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
+            data-modal-target={`static-modal-${key}`}
+            data-modal-toggle={`static-modal-${key}`}
+          >
             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
               Apply
             </span>
@@ -79,6 +161,7 @@ const Opportunities = () => {
       <NavBar />
       <div className="container mx-auto">
         {/* Opportunities Container */}
+
         <div className="grid md:grid-cols-2 gap-5 my-10 md:mx-0 mx-2">
           {/* {opportunities.map((val, key) => {
             return opportunity(val, key);
