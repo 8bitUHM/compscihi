63a19bbdc74d27d3d365525882ba17055fc619d1
Leighton Miguel <leim56@hawaii.rr.com>
2024-09-22 15:37:17 -1000

    Logged in button refactor

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -97,12 +97,17 @@ const NavBar: FC = () => {
                       d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
                     />
                   </svg>
-                  {!user ? "Profile" : user.email}
+                  {!user ? "" : `${user.email.split("@")[0].slice(0, 6)}`}
                 </button>
                 <div
                   className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
                   id="user-dropdown"
                 >
+                  <div className="px-4 py-3">
+                    <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
+                      {!user ? "" : user.email}
+                    </span>
+                  </div>
                   <ul className="py-2" aria-labelledby="user-menu-button">
                     <li>
                       <a
