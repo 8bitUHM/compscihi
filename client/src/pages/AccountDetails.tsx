25b0f33091fe89d2f1044d63ec8750294b1df03f
Leighton Miguel <leim56@hawaii.rr.com>
2024-09-23 16:01:19 -1000

    Resend verification component

--- client/src/pages/AccountDetails.tsx
+++ client/src/pages/AccountDetails.tsx
@@ -7,6 +7,7 @@ import "../styles/styles.css";
 import { initFlowbite } from "flowbite";
 import { auth, getAccountType } from "../firebase/firebase";
 import { onAuthStateChanged, sendEmailVerification } from "firebase/auth";
+import ResendVerification from "../components/ResendVerification";
 
 const AccountDetails = () => {
   const [email, setEmail] = useState<null | string>(null);
@@ -32,7 +33,7 @@ const AccountDetails = () => {
         setUserVerified(user.emailVerified);
         setLoading(false);
       } else {
-        setLoggedIn(false)
+        setLoggedIn(false);
         setLoading(false);
       }
     });
@@ -99,48 +100,7 @@ const AccountDetails = () => {
                       </div>
                     </section>
                   ) : (
-                    <section className="bg-white dark:bg-gray-900">
-                      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-                        <div className="mx-auto max-w-screen-sm text-center">
-                          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-teal-500 dark:text-primary-500">
-                            401
-                          </h1>
-                          <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
-                            Your account is not yet verified.
-                          </p>
-                          <small>
-                            Verify your account with the link in your email or
-                            re-send the verification below and refresh the page.
-                          </small>
-
-                          <button
-                            onClick={() => {
-                              try {
-                                sendEmailVerification(auth.currentUser);
-                                setVerificationSendFeedbackSuccess(
-                                  "Verification link sent successfully!"
-                                );
-                              } catch (e) {
-                                setVerificationSendFeedbackFailure(
-                                  `Failed to send verification link, ${e}, please refresh the page and try again.`
-                                );
-                              }
-                            }}
-                            className="inline-flex text-white bg-teal-500 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
-                          >
-                            Re-send verification link
-                          </button>
-                          <br></br>
-                          <small className="text-green-500">
-                            {verificationSendFeedbackSuccess}
-                          </small>
-                          <br></br>
-                          <small className="text-red-500">
-                            {verificationSendFeedbackFailure}
-                          </small>
-                        </div>
-                      </div>
-                    </section>
+                    <ResendVerification />
                   )}
                 </div>
               </>
