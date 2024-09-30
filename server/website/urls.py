3bbb4866ab0a983c5c88ca60b3258a2c17ca256e
Leighton Miguel <leim56@hawaii.rr.com>
2024-09-30 01:33:05 -1000

    Re-send verification

--- server/website/urls.py
+++ server/website/urls.py
@@ -1,8 +1,9 @@
 # example/urls.py
 from django.urls import path
 
-from website.views import signup
+from website.views import signup, resend_verification_email
 
 urlpatterns = [
   path('signup/', signup, name='signup'),
+  path('resend-verification/', resend_verification_email, name='resend-verification'),
 ]
--- server/website/views.py
+++ server/website/views.py
@@ -1,12 +1,11 @@
 # example/views.py
 from django.contrib.auth.views import LoginView, LogoutView
-from .forms import CustomUserCreationForm
-from django.shortcuts import render, get_object_or_404, redirect
+from .forms import CustomUserCreationForm, ResendVerificationForm
+from django.shortcuts import render
 from .models import *
 from .forms import CustomUserCreationForm, LoginForm
 from django_email_verification import send_email
-from django.urls import reverse_lazy
-from django.contrib import messages
+from django.contrib.auth.models import User
 
 def signup(request):
   if request.method == 'POST':
@@ -30,3 +29,17 @@ class WebsiteLoginView(LoginView):
   
 class WebsiteLogoutView(LogoutView):
   next_page = '/admin'
+  
+def resend_verification_email(request):
+  if request.method == 'POST':
+    form = ResendVerificationForm(request.POST)
+    if form.is_valid():
+      email = form.cleaned_data['email']
+      user = User.objects.get(email = email)
+      send_email(user)
+      form.errors['success'] = "Success"
+    
+  else:
+    form = ResendVerificationForm()
+
+  return render(request, 'pages/resend-verification.html', {'form': form})
