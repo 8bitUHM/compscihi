f1618e01a211dd0ba610d98636b01ee9fd5630aa
Leighton Miguel <leim56@hawaii.rr.com>
2024-10-07 20:05:03 -1000

    Remove bloat

--- client/src/pages/Opportunities.tsx
+++ client/src/pages/Opportunities.tsx
@@ -363,30 +363,6 @@ const Opportunities = () => {
                 {sortOrder === "asc" ? " " : " "}
               </button>
 
-              <div
-                id="orderDropdown"
-                className="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow-xl w-44 dark:bg-gray-700 dark:divide-gray-600"
-              >
-                <ul
-                  className="py-1 text-sm text-gray-700 dark:text-gray-200"
-                  aria-labelledby="actionsDropdownButton"
-                >
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    ></a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Descending
-                    </a>
-                  </li>
-                </ul>
-              </div>
               <button
                 id="sortDropdownButton"
                 data-dropdown-toggle="sortDropdown"
@@ -488,41 +464,6 @@ const Opportunities = () => {
                         </svg>
                       )}
                     </button>
-                  </li>
-                </ul>
-              </div>
-
-              <div
-                id="sortDropdown"
-                className="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow-xl w-44 dark:bg-gray-700 dark:divide-gray-600"
-              >
-                <ul
-                  className="py-1 text-sm text-gray-700 dark:text-gray-200"
-                  aria-labelledby="actionsDropdownButton"
-                >
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Date posted
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Name
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Salary
-                    </a>
                   </li>
                 </ul>
               </div>
