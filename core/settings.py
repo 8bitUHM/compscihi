58f46cc539ce5fbccf43d9513ada7d8186e04fdf
Leighton Miguel <leim56@hawaii.rr.com>
2024-11-12 21:39:39 -1000

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'wasmeti3km.us-west-2.awsapprunner.com', 'portal.compscihi.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'wasmeti3km.us-west-2.awsapprunner.com', 'portal.compscihi.com', 'compscihi.com']
 
 
 # Application definition
@@ -421,6 +421,6 @@ EMAIL_USE_TLS = True
 
 DEFAULT_FROM_EMAIL = 'noreply<no_reply@domain.com>'
 
-CSRF_TRUSTED_ORIGINS = ['https://portal.compscihi.com']
+CSRF_TRUSTED_ORIGINS = ['https://compscihi.com']
 
 
